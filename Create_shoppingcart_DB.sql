7375fe9e543d05a056ca2ff1e465dd33582ce726
Daniel Goss <gosshawk2@yahoo.co.uk>
2021-04-23 15:45:13 +0100

    Create fruit Shopping Cart DB Setup

---
+++ Create_shoppingcart_DB.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS `shoppingcart`;
+CREATE DATABASE IF NOT EXISTS `shoppingcart`;
+USE `shoppingcart`;
+
+-- Dumping structure for table shoppingcart.tblproducts
+DROP TABLE IF EXISTS `tblproducts`;
+CREATE TABLE IF NOT EXISTS `tblproducts` (
+  `ProductID` int(11) NOT NULL AUTO_INCREMENT,
+  `ProductCode` varchar(50) NOT NULL DEFAULT '0',
+  `ProductName` varchar(50) NOT NULL DEFAULT '0',
+  `Price` double(10,2) NOT NULL DEFAULT 0.00,
+  `imagepath` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`ProductID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `tblproducts` (`ProductID`, `ProductCode`, `ProductName`, `Price`, `imagepath`) VALUES
+	(1, 'App001', 'Green Apple', 0.60, 'Product-images/Green-Apple.jpg'),
+	(2, 'Ora001', 'Orange', 0.25, 'Product-images/Orange.jpg');
+    
+
